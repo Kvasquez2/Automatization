4ee2e71cbe97ff3c08a73f5eab36a99569791df8
Kevin Vasquez <66072723+Kvasquez2@users.noreply.github.com>
2024-08-15 14:12:59 -0500

    Delete Selenium-Basic directory

--- Selenium-Basic/src/test/java/NewAfiliado.java
+++
@@ -1,8 +0,0 @@
-public class NewAfiliado {
-    public static void main(String[] args) {
-        ConsultaBD consultaBD = new ConsultaBD(); // Crear una instancia de ConsultaBD
-        //consultaBD.realizarConsulta(); // Llamar al método para realizar la consulta
-    }
-}
-
-
